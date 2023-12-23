af399fc0028daa7d2c797f0c518f81bb2eafe2b7
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-12-22 22:15:55 -0500

    1222

---
+++ christmasproject/script.js
@@ -0,0 +1,5 @@
+function changeGreeting() {
+  const button = document.getElementById('christmasButton');
+  button.innerHTML = 'MERRY CHRISTMAS 17th DADDY AND MOMMY';
+  document.body.style.background = "url('christmasScene.jpg') center/cover no-repeat"; /* Set your new Christmas scene background image */
+}
